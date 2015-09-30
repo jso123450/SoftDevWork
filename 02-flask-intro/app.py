bb9f31fe4906080cd3a1ad7cb082949e9a21c99a
Johnny So <jso123450@gmail.com>
2015-09-30 11:48:19 -0400

    Working on Sessions

--- 02-flask-intro/app.py
+++ 02-flask-intro/app.py
@@ -1,8 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 import utils
 app = Flask(__name__)
 
 # define the directory for the app
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method=="GET":
+        return render_template("login.html")
+    else:
+        button = request.form['button']
+        un = request.form['username']
+        pw = request.form['password']
+        if button="logout":
+            session['un'] = 0
+            session['pw'] = 0
+            return render_template("login.html")
+        else:
+            if utils.loginauth(un,pw):
+                session['un'] = un
+                session['pw'] = pw
+                return redirect(url_for("home"))
+            else:
+                error = "INVALID USERNAME AND/OR PASSWORD"
+                return render_template("login.html",error=error)
+
 @app.route("/about")
 def about():
     return render_template("about.html")
@@ -16,6 +37,10 @@ def lucky_number():
 @app.route("/artist/",methods=["GET","POST"])
 #@app.route("/artist/<name>",methods=["GET","POST"])
 def artist(name=""):
+    if 'un' not in session:
+        error = "You are not logged in"
+        return redirect(url_for("home"))
+        #return render_template("home.html",error=error)
     d = utils.returnd()
     d2 = utils.returnartists()
     if request.method=="GET":
@@ -36,5 +61,6 @@ def home():
 
 if __name__ == "__main__":
     app.debug = True
+    app.secret_key="hello world"
     app.run(host='0.0.0.0',port=8000)
     # accessible on 'localhost:8000'
