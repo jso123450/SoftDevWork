2a8d204b5c6ac66f60e21e77096a48a8e178cf82
Johnny So <jso123450@gmail.com>
2015-12-03 11:36:51 -0500

    Renamed

---
+++ 04-javascript-todo/todolist.js
@@ -0,0 +1,71 @@
+console.log("JavaScript successfully loaded");
+
+var addToTDL = function addToTDL(s){
+    var tdl = document.getElementById("todolist");
+    var n = document.createElement("li");
+    n.innerHTML = s;
+    tdl.appendChild(n);
+    var tdlItems = todolist.children;
+    var length = tdlItems.length;
+    tdlItems[length-1].addEventListener("click", function(){moveToDL(length-1)});
+};
+
+var removeFromTDL = function removeFromTDL(n){
+    var items = todolist.children;
+    items[n].remove();
+};
+
+var moveToDL = function moveToDL(n){
+    console.log("hello")
+    var tdlItem = document.getElementsByTagName("li")[n];
+    var dl = document.getElementById("donelist");
+    var doneItem = document.createElement("li");
+    doneItem.innerHTML = tdlItem.innerHTML;
+    dl.appendChild(doneItem);
+    removeFromTDL(n);
+    var dlItems = donelist.children;
+    var length = dlItems.length;
+    dlItems[length-1].addEventListener("click",function(){moveToTDL(length-1)});
+};
+
+var removeFromDL = function removeFromDL(n){
+    var items = donelist.children;
+    items[n].remove();
+};
+
+var moveToTDL = function moveToTDL(n){
+    var dlItem = document.getElementsByTagName("li")[n];
+    var tdl = document.getElementById("todolist");
+    var todoItem = document.createElement("li");
+    todoItem.innerHTML = dlItem.innerHTML;
+    tdl.appendChild(todoItem);
+    removeFromDL(n);
+    var tdlItems = todolist.children;
+    var length = tdlItems.length;
+    tdlItems[length-1].addEventListener("click",function(){moveToDL(length-1)});
+}
+
+var buttonCallBack = function buttonCallBack(e){
+    console.log(e);
+    console.log(this);
+    var input = document.getElementById("TDLinput");
+    var text = input.value;
+    addToTDL(text);
+};
+
+var highlightedItem = 0;
+
+var highlightButtonCallBack = function highlightButtonCallBack(){
+    var tdlItems = todolist.children;
+    tdlItems[highlightedItem].classList.toggle("liRed");
+    if (highlightedItem > 0){
+	tdlItems[highlightedItem].classList.toggle("liRed");
+    };
+    highlightedItem++;
+};
+
+var addButton = document.getElementById("addButton");
+addButton.addEventListener("click",buttonCallBack);
+
+var highlightButton = document.getElementById("highlightButton");
+highlightButton.addEventListener("click",highlightButtonCallBack());
